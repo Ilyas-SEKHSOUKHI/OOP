9c4e7800bdab2a326fcdb1d11a84fbf9ef595846
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-09-25 20:52:09 +0100

    Premiere seance

---
+++ helloworld.java
@@ -0,0 +1,31 @@
+public class helloworld{
+    public static void main (String[] args){
+        System.out.println("Bonjour , le JDK 25 fonctionne !");
+        //variable
+        int a = 12 ;
+        double b = 2.2;
+        String c = "hello world";
+        boolean d = true;
+        //-------------------------------
+
+
+       /*  double v1 = 5.5;
+        Double v2 = new Double(5.6); */
+
+
+        //operation
+        int somme = a + (int)b;
+        int soustraction = a - (int)b;
+        int multuplication = a * (int)b;
+
+       // int SommeV1V2 = v1 + v2;
+
+        //affichage
+        System.out.println(somme);
+        System.out.println(soustraction);
+        System.out.println(multuplication);
+        
+        // System.out.println(SommeV1V2);
+
+    }
+}
