b173f3fe2b3f995544d0c2493306ba8bb8db044f
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-13 19:24:13 +0100

    modif

--- Troisieme_Seance/TP2_OOP/Toto.java
+++ Troisieme_Seance/TP2_OOP/Toto.java
@@ -4,8 +4,11 @@ package Troisieme_Seance.TP2_OOP;
     class Toto{ int toto =0;Toto(){
         toto = toto +1;
     }
-    public static void main(String[] args) { Toto t1 = new Toto();
-        Toto t2 = new Toto();System.out.println("Toto : "+t1.toto);
+    public static void main(String[] args) {
+  
+        Toto t1 = new Toto();
+        Toto t2 = new Toto();
+        System.out.println("Toto : "+t1.toto);
     }
 
     }
