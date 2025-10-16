91600149d8057b468a0669ca9de7803db9e91a20
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-16 16:29:04 +0100

    EX4 CompteBancaire class 4Seance

---
+++ Quatrieme Seance/TP/EX3/CompteBancaire.java
@@ -0,0 +1,49 @@
+package EX3;
+
+public class CompteBancaire {
+    String titulaire;
+    double soldes;
+    public CompteBancaire(String titulaire,double soldes){
+        this.titulaire = titulaire;
+        this.soldes = soldes;
+    } 
+    public String getTitulaire(){
+        return titulaire;
+    }
+    public void setTitulaire(String titulaire){
+        this.titulaire = titulaire;
+    }
+    public double getSoldes(){
+        return soldes;
+    }
+    public void setSoldes(double soldes){
+        this.soldes = soldes;
+    }
+    public double Deposer(double A){
+        double Totale = A+soldes;
+        return Totale;
+        
+    }
+    public double Retirer(double B){
+        double Totale = soldes-B;
+        return Totale;
+    }
+    public double AppliquerFrais(double Frais){
+        double A = (Frais*soldes)/100;
+        double Totale = soldes-A;
+        return Totale;
+    }
+    public double AppliquerInterets(double Interets){
+        double A = (Interets*soldes)/100;
+        double Totale = soldes+A;
+        return Totale;
+    }
+    public void Affichage(CompteBancaire A){
+        System.out.println("Titulaire = "+A.getTitulaire());
+        System.out.println("Soldes = "+A.getSoldes());
+        System.out.println("Deposer = "+A.Deposer(500));
+        System.out.println("Retirer = "+A.Retirer(200));
+        System.out.println("Appliquer Frais = "+A.AppliquerFrais(20));
+        System.out.println("Appliquer Interets = "+A.AppliquerInterets(20));
+    }
+}
---
+++ Quatrieme Seance/TP/EX3/main.java
@@ -0,0 +1,9 @@
+package EX3;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        CompteBancaire A = new CompteBancaire("Ilyas SEKHSOUKHI", 70000);
+        Affichage(A); // Probleme a resoudre
+    }
+}
