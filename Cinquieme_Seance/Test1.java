09dcdcbb6e64d5fd843211d7de389a6a91963fda
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-21 16:38:23 +0100

    Cinquieme seance

---
+++ Cinquieme_Seance/Test1.java
@@ -0,0 +1,17 @@
+package Cinquieme_Seance;
+
+class A {
+    A(){
+        System.out.println("Constructeur A");
+    }
+}
+class B extends A {
+    B(){
+        System.out.println("Constructeur B");
+    }
+}
+public class Test1 {
+    public static void main(String[] args) {
+        B obj = new B();
+    }
+}
