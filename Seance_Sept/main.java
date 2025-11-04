53ff5e3e92ded75387c9bec825ce6af7e8e53bf2
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-11-04 16:05:02 +0100

    modif

--- Seance_Sept/main.java
+++ Seance_Sept/main.java
@@ -2,6 +2,6 @@ package JAVA.Seance_Sept;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Seance 7 Interface / Heritage");
     }
 }
